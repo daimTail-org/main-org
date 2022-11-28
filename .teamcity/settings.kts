c0f0b3b6664efb80016ae24e99850e38d48dd180
dts <dts@TeamCity>
2022-11-28 18:42:15 +0400

    TeamCity change in 'mdts' project: 'name-of-the-build' build configuration settings were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -56,7 +56,6 @@ object Build : BuildType({
 
     features {
         approval {
-            enabled = false
             approvalRules = """
                 group:qwa:2
                 group:tleads:1
